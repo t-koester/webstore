e89cd8e2b6e9b2ede46fc4d1cba46dcf41b54da0
t-koester <173665346+t-koester@users.noreply.github.com>
2024-06-25 11:32:02 +0200

    Added Slidebar

--- header.php
+++ header.php
@@ -3,4 +3,11 @@
         <div>
             <h1>Impsum Online Shop</h1>
     </div>
+    <div class="topnav">
+  <a href="http://localhost:8000/index.php">Home</a>
+  <a href="http://localhost:8000/products.php">Shop</a>
+  <a href="http://localhost:8000/kontakt.php">Kontakt</a>
+  <a href="http://localhost:8000/PLACEHOLDER">News</a>
+  <a href="http://localhost:8000/impressum.php">Impressum</a>
+</div> 
 </header>
