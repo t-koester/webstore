895ac7ca94ab72b0e06cbd6f12a6c0e9fde73f65
t-koester <173665346+t-koester@users.noreply.github.com>
2024-06-25 09:27:53 +0200

    Started first product page

---
+++ products/Product1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Product Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Simple Product Page</h1>
+    </header>
+    <main>
+        <div class="product2">
+        <img src="https://i.ibb.co/cXjCvW1/product.png" alt="product-image"> 
+            </div>
+        </div>
+    </main>
+</body>
+</html>
