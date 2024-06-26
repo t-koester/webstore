b0bf508438aa78b24d379ada4aadbb81a7fa6cf8
t-koester <173665346+t-koester@users.noreply.github.com>
2024-06-26 13:36:42 +0200

    Added About US Page

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<?php include 'head.php' ?>
+<body>
+    <?php include 'header.php' ?>
+    <div class="abt_us-titel">
+                <h2>Wir sind soooo Tool weil:</h2>
+            </div>
+            <div class="abt_us-box">
+                
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
+    <?php include 'footer.php' ?>
+</body>
